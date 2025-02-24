P3nguinMinecraft/SkytilsModFork

commit 816984225f92fa4174e2fca027ba36c7c3f59684

feat(Catlas): More optimized core scanning
https://github.com/Harry282/FunnyMap/commit/44a1958158a2c1849a9d465891c1374da591b299

Co-authored-by: Harry282 <48564154+Harry282@users.noreply.github.com>

src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/catlas/utils/ScanUtils.kt
@@ -65,12 +65,25 @@ object ScanUtils {
 
     fun getCore(x: Int, z: Int): Int {
         val sb = StringBuilder(150)
-        val chunk = mc.theWorld.getChunkFromBlockCoords(BlockPos(x, 0, z))
-        for (y in 140 downTo 12) {
-            val id = Block.blockRegistry.getIDForObject(chunk.getBlock(BlockPos(x, y, z)))
-            if (!Utils.equalsOneOf(id, 5, 54, 146)) {
-                sb.append(id)
+        val chunk = mc.theWorld.getChunkFromChunkCoords(x shr 4, z shr 4)
+        val height = chunk.getHeightValue(x and 15, z and 15).coerceIn(11..140)
+        sb.append(CharArray(140 - height) { '0' })
+        var bedrock = 0
+        for (y in height downTo 12) {
+            val id = Block.getIdFromBlock(chunk.getBlock(BlockPos(x, y, z)))
+            if (id == 0 && bedrock >= 2 && y < 69) {
+                sb.append(CharArray(y - 11) { '0' })
+                break
             }
+
+            if (id == 7) {
+                bedrock++
+            } else {
+                bedrock = 0
+                if (!Utils.equalsOneOf(id, 5, 54, 146)) continue
+            }
+
+            sb.append(id)
         }
         return sb.toString().hashCode()
     }

commit f25a957d8da17f23028b8165c284ee2f12b15c6d

fix(Catlas): incorrect core scanning logic
fixes bug introduced in 816984225f92fa4174e2fca027ba36c7c3f59684

src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/catlas/utils/ScanUtils.kt
@@ -80,7 +80,7 @@ object ScanUtils {
                 bedrock++
             } else {
                 bedrock = 0
-                if (!Utils.equalsOneOf(id, 5, 54, 146)) continue
+                if (Utils.equalsOneOf(id, 5, 54, 146)) continue
             }
 
             sb.append(id)