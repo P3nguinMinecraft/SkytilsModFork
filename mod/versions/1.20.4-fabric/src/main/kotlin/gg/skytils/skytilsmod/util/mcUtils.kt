4696d649e640769839897cf5c2931b78e4f58c56
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2025-04-15 16:35:02 -0400

    refactor: fix incorrect package name

--- mod/versions/1.20.4-fabric/src/main/kotlin/gg/skytils/skytilsmod/util/mcUtils.kt
+++ mod/versions/1.20.4-fabric/src/main/kotlin/gg/skytils/skytilsmod/util/mcUtils.kt
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package gg.sktyils.skytilsmod.util
+package gg.skytils.skytilsmod.util
 
 import net.minecraft.text.Text
 
