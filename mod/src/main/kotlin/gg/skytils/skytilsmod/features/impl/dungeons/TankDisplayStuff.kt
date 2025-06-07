a0b742eabc755c4d5030569945ded811f06c5104
sychic <47618543+Sychic@users.noreply.github.com>
2025-06-07 13:36:20 -0400

    refactor: fix errors in TankDisplayStuff
    [no ci]

--- mod/src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/TankDisplayStuff.kt
+++ mod/src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/TankDisplayStuff.kt
@@ -42,83 +42,84 @@ object TankDisplayStuff : EventSubscriber {
         for (teammate in DungeonListener.team.values) {
             val player = teammate.player ?: continue
             if (!teammate.canRender()) continue
-            if (teammate.dungeonClass == DungeonClass.TANK) {
-                if (Skytils.config.showTankRadius) {
-                    // not sba healing circle wall code
-                    RenderSystem.pushMatrix()
-                    GL11.glNormal3f(0.0f, 1.0f, 0.0f)
-
-                    RenderSystem.method_4406()
-                    RenderSystem.depthMask(false)
-                    RenderSystem.enableDepthTest()
-                    RenderSystem.enableBlend()
-                    RenderSystem.depthFunc(GL11.GL_LEQUAL)
-                    RenderSystem.disableCull()
-                    RenderSystem.blendFuncSeparate(770, 771, 1, 0)
-                    RenderSystem.method_4456()
-                    RenderSystem.method_4407()
-
-                    if (Skytils.config.showTankRadiusWall) {
-                        Skytils.config.tankRadiusDisplayColor.bindColor()
-                        RenderUtil.drawCylinderInWorld(
-                            player.x,
-                            player.y - 30,
-                            player.z,
-                            30f,
-                            60f,
-                            event.partialTicks
-                        )
-                    } else {
-                        RenderSystem.disableDepthTest()
-                        RenderUtil.drawCircle(
-                            player,
-                            event.partialTicks,
-                            30.0,
-                            Skytils.config.tankRadiusDisplayColor
-                        )
-                        RenderSystem.enableDepthTest()
-                    }
-
-                    RenderSystem.enableCull()
-                    RenderSystem.method_4397()
-                    RenderSystem.enableDepthTest()
-                    RenderSystem.depthMask(true)
-                    RenderSystem.method_4394()
-                    RenderSystem.disableBlend()
-                    RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f)
-                    RenderSystem.popMatrix()
-                }
-                if (Skytils.config.boxedTanks && (teammate.player != mc.player || mc.options.perspective != 0)) {
-                    RenderSystem.disableCull()
-                    RenderSystem.disableDepthTest()
-                    RenderUtil.drawOutlinedBoundingBox(
-                        player.boundingBox,
-                        Skytils.config.boxedTankColor,
-                        2f,
-                        1f
-                    )
-                    RenderSystem.enableDepthTest()
-                    RenderSystem.enableCull()
-                }
-            }
-            if (Skytils.config.boxedProtectedTeammates && (player != mc.player || mc.options.perspective != 0)) {
-                if (DungeonListener.team.values.any {
-                        it.canRender() && it.dungeonClass == DungeonClass.TANK && it != teammate && it.player?.distanceTo(
-                            player
-                        )!! <= 30
-                    }) {
-                    RenderSystem.disableCull()
-                    RenderSystem.disableDepthTest()
-                    RenderUtil.drawOutlinedBoundingBox(
-                        player.boundingBox,
-                        Skytils.config.boxedProtectedTeammatesColor,
-                        2f,
-                        1f
-                    )
-                    RenderSystem.enableDepthTest()
-                    RenderSystem.enableCull()
-                }
-            }
+            // TODO: fix later (with render pipeline)
+//            if (teammate.dungeonClass == DungeonClass.TANK) {
+//                if (Skytils.config.showTankRadius) {
+//                    // not sba healing circle wall code
+//                    RenderSystem.pushMatrix()
+//                    GL11.glNormal3f(0.0f, 1.0f, 0.0f)
+//
+//                    RenderSystem.method_4406()
+//                    RenderSystem.depthMask(false)
+//                    RenderSystem.enableDepthTest()
+//                    RenderSystem.enableBlend()
+//                    RenderSystem.depthFunc(GL11.GL_LEQUAL)
+//                    RenderSystem.disableCull()
+//                    RenderSystem.blendFuncSeparate(770, 771, 1, 0)
+//                    RenderSystem.method_4456()
+//                    RenderSystem.method_4407()
+//
+//                    if (Skytils.config.showTankRadiusWall) {
+//                        Skytils.config.tankRadiusDisplayColor.bindColor()
+//                        RenderUtil.drawCylinderInWorld(
+//                            player.x,
+//                            player.y - 30,
+//                            player.z,
+//                            30f,
+//                            60f,
+//                            event.partialTicks
+//                        )
+//                    } else {
+//                        RenderSystem.disableDepthTest()
+//                        RenderUtil.drawCircle(
+//                            player,
+//                            event.partialTicks,
+//                            30.0,
+//                            Skytils.config.tankRadiusDisplayColor
+//                        )
+//                        RenderSystem.enableDepthTest()
+//                    }
+//
+//                    RenderSystem.enableCull()
+//                    RenderSystem.method_4397()
+//                    RenderSystem.enableDepthTest()
+//                    RenderSystem.depthMask(true)
+//                    RenderSystem.method_4394()
+//                    RenderSystem.disableBlend()
+//                    RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f)
+//                    RenderSystem.popMatrix()
+//                }
+//                if (Skytils.config.boxedTanks && (teammate.player != mc.player || mc.options.perspective != 0)) {
+//                    RenderSystem.disableCull()
+//                    RenderSystem.disableDepthTest()
+//                    RenderUtil.drawOutlinedBoundingBox(
+//                        player.boundingBox,
+//                        Skytils.config.boxedTankColor,
+//                        2f,
+//                        1f
+//                    )
+//                    RenderSystem.enableDepthTest()
+//                    RenderSystem.enableCull()
+//                }
+//            }
+//            if (Skytils.config.boxedProtectedTeammates && (player != mc.player || mc.options.perspective != 0)) {
+//                if (DungeonListener.team.values.any {
+//                        it.canRender() && it.dungeonClass == DungeonClass.TANK && it != teammate && it.player?.distanceTo(
+//                            player
+//                        )!! <= 30
+//                    }) {
+//                    RenderSystem.disableCull()
+//                    RenderSystem.disableDepthTest()
+//                    RenderUtil.drawOutlinedBoundingBox(
+//                        player.boundingBox,
+//                        Skytils.config.boxedProtectedTeammatesColor,
+//                        2f,
+//                        1f
+//                    )
+//                    RenderSystem.enableDepthTest()
+//                    RenderSystem.enableCull()
+//                }
+//            }
         }
     }
 }
