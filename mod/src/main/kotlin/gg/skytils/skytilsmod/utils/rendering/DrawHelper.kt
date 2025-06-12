9e842d2af0b4365a182dff95fb9af8e597ae5dfd
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2025-06-12 11:50:39 -0400

    feat: Create DrawHelper#drawTexture

--- mod/src/main/kotlin/gg/skytils/skytilsmod/utils/rendering/DrawHelper.kt
+++ mod/src/main/kotlin/gg/skytils/skytilsmod/utils/rendering/DrawHelper.kt
@@ -18,13 +18,17 @@
 
 package gg.skytils.skytilsmod.utils.rendering
 
+import com.mojang.blaze3d.systems.RenderSystem
 import gg.essential.universal.UMatrixStack
 import gg.essential.universal.vertex.UBufferBuilder
 import gg.skytils.skytilsmod.Skytils.mc
+import net.minecraft.util.Identifier
 import net.minecraft.util.math.Box
 import java.awt.Color
 
 object DrawHelper {
+    private val textureManager = mc.textureManager
+
     /**
      * Applies the camera offset to the given matrices.
      * This is useful for rendering things in world space, as it will negate the camera position.
@@ -135,5 +139,39 @@ object DrawHelper {
             buffer.pos(matrices, minX, maxY, minZ).color(color).endVertex()
             buffer.pos(matrices, minX, minY, minZ).color(color).endVertex()
         }
+    }
+
+
+    /**
+     * Writes a texture quad to the buffer. Draw must still be called manually.
+     * Buffer must be created with [gg.essential.universal.UGraphics.DrawMode.QUADS]
+     */
+    fun drawTexture(
+        matrices: UMatrixStack,
+        buffer: UBufferBuilder,
+        sprite: Identifier,
+        x: Double,
+        y: Double,
+        u: Double = 0.0,
+        v: Double = 0.0,
+        width: Double,
+        height: Double,
+        textureWidth: Double = width,
+        textureHeight: Double = height,
+        color: Color = Color.WHITE
+    ) {
+        val abstractTexture = textureManager.getTexture(sprite)
+        abstractTexture.setFilter(false, false)
+        RenderSystem.setShaderTexture(0, abstractTexture.glTexture)
+        val x2 = x + width
+        val y2 = y + height
+        val u1 = u / textureWidth
+        val u2 = (u + width) / textureWidth
+        val v1 = v / textureHeight
+        val v2 = (v + height) / textureHeight
+        buffer.pos(matrices, x, y, 0.0).tex(u1, v1).color(color).endVertex()
+        buffer.pos(matrices, x, y2, 0.0).tex(u1, v2).color(color).endVertex()
+        buffer.pos(matrices, x2, y2, 0.0).tex(u2, v2).color(color).endVertex()
+        buffer.pos(matrices, x2, y, 0.0).tex(u2, v1).color(color).endVertex()
     }
 }
