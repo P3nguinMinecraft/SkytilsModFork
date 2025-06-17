fcd7f286e2f9caaccdc6746412c23aa7c9dc180b
sychic <47618543+Sychic@users.noreply.github.com>
2025-06-17 00:01:42 -0400

    refactor: fix straggling import
    [no ci]

--- mod/src/main/kotlin/gg/skytils/skytilsmod/gui/elements/GIFResource.kt
+++ mod/src/main/kotlin/gg/skytils/skytilsmod/gui/elements/GIFResource.kt
@@ -21,7 +21,6 @@ package gg.skytils.skytilsmod.gui.elements
 import gg.skytils.skytilsmod.Skytils
 import gg.skytils.skytilsmod.Skytils.mc
 import gg.skytils.skytilsmod.utils.RenderUtil
-import gg.skytils.skytilsmod.utils.graphics.DynamicResource
 import gg.skytils.skytilsmod.utils.nextOrNull
 import kotlinx.coroutines.launch
 import net.minecraft.util.Identifier
