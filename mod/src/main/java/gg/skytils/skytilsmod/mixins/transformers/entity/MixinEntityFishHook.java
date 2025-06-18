1be22fe695fb3d7ed2218305bc00b9571aefdfe9
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2025-06-18 14:16:55 -0400

    chore: update `MixinEntityFishHook`

--- mod/src/main/java/gg/skytils/skytilsmod/mixins/transformers/entity/MixinEntityFishHook.java
+++ mod/src/main/java/gg/skytils/skytilsmod/mixins/transformers/entity/MixinEntityFishHook.java
@@ -22,25 +22,20 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import gg.skytils.skytilsmod.Skytils;
 import gg.skytils.skytilsmod.utils.Utils;
-import net.minecraft.block.Material;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
-import net.minecraft.util.math.Box;
-import net.minecraft.world.World;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.registry.tag.TagKey;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(FishingBobberEntity.class)
-public abstract class MixinEntityFishHook extends Entity {
-
-    public MixinEntityFishHook(World worldIn) {
-        super(worldIn);
-    }
-
-    @WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;method_0_363(Lnet/minecraft/util/math/Box;Lnet/minecraft/block/Material;)Z"))
-    private boolean allowLavaBobber(World world, Box aabb, Material materialIn, Operation<Boolean> original) {
-        boolean orig = original.call(world, aabb, materialIn);
+public abstract class MixinEntityFishHook {
+    @WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/fluid/FluidState;isIn(Lnet/minecraft/registry/tag/TagKey;)Z"))
+    private boolean allowLavaBobber(FluidState instance, TagKey<Fluid> tag, Operation<Boolean> original) {
+        boolean orig = original.call(instance, tag);
         if (!Utils.INSTANCE.getInSkyblock() || !Skytils.getConfig().getLavaBobber()) return orig;
-        return this.world.method_0_363(aabb, Material.LAVA) || orig;
+        return orig || instance.isIn(FluidTags.LAVA);
     }
 }
