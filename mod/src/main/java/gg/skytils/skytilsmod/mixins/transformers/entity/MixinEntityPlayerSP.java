a10486e79d13669ff34ca8c5e6f4ee357afd92ae
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2025-06-20 17:14:05 -0400

    chore: fix errors in MixinEntityPlayerSP
    [no ci]

--- mod/src/main/java/gg/skytils/skytilsmod/mixins/transformers/entity/MixinEntityPlayerSP.java
+++ mod/src/main/java/gg/skytils/skytilsmod/mixins/transformers/entity/MixinEntityPlayerSP.java
@@ -21,30 +21,21 @@ package gg.skytils.skytilsmod.mixins.transformers.entity;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.mojang.authlib.GameProfile;
 import gg.skytils.skytilsmod.mixins.hooks.entity.EntityPlayerSPHookKt;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.text.Text;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerEntity.class)
-public abstract class MixinEntityPlayerSP extends AbstractClientPlayerEntity {
+public abstract class MixinEntityPlayerSP {
     @Shadow
-    protected MinecraftClient client;
-
-    public MixinEntityPlayerSP(World worldIn, GameProfile playerProfile) {
-        super(worldIn, playerProfile);
-    }
+    protected final MinecraftClient client = null;
 
     @Inject(method = "dropSelectedItem", at = @At("HEAD"), cancellable = true)
     private void onDropItem(boolean dropAll, CallbackInfoReturnable<ItemEntity> cir) {
