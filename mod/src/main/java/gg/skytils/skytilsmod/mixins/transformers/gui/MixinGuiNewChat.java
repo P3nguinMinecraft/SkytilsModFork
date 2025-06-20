85ec3b7b4fcf2f858488108ac23cb66ce17522dc
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2025-06-20 17:24:00 -0400

    chore: Fix errors in `MixinGuiNewChat`
    [no ci]

--- mod/src/main/java/gg/skytils/skytilsmod/mixins/transformers/gui/MixinGuiNewChat.java
+++ mod/src/main/java/gg/skytils/skytilsmod/mixins/transformers/gui/MixinGuiNewChat.java
@@ -18,16 +18,13 @@
 
 package gg.skytils.skytilsmod.mixins.transformers.gui;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import gg.skytils.skytilsmod.features.impl.handlers.ChatTabs;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHudLine;
-import net.minecraft.client.TextRenderer;
 import net.minecraft.client.gui.hud.ChatHud;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.class_0_686;
-import net.minecraft.text.LiteralTextContent;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -45,43 +42,48 @@ import java.util.Iterator;
 import java.util.List;
 
 @Mixin(value = ChatHud.class, priority = 1001)
-public abstract class MixinGuiNewChat extends DrawContext {
-
-
-    private static final ChatHudLine skytils$placeholderLine = new ChatHudLine(0, new LiteralTextContent("skytils placeholder"), 0);
+public abstract class MixinGuiNewChat {
 
     @Shadow
-    @Final
-    private MinecraftClient client;
+    private final MinecraftClient client = null;
 
     @Shadow
     public abstract int getVisibleLineCount();
 
-    @WrapOperation(method = "addMessage(Lnet/minecraft/text/Text;IIZ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/ChatMessages;breakRenderedChatMessageLines(Lnet/minecraft/text/Text;ILnet/minecraft/client/TextRenderer;ZZ)Ljava/util/List;"))
-    private List<Text> filterDrawnTextComponents(Text p_178908_0_, int p_178908_1_, TextRenderer p_178908_2_, boolean p_178908_3_, boolean p_178908_4_, Operation<List<Text>> original) {
-        return ChatTabs.INSTANCE.shouldAllow(p_178908_0_) ? original.call(p_178908_0_, p_178908_1_, p_178908_2_, p_178908_3_, p_178908_4_) : Collections.emptyList();
+    //#if MC==10809
+    //$$ @WrapOperation(method = "addMessage(Lnet/minecraft/text/Text;IIZ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/ChatMessages;breakRenderedChatMessageLines(Lnet/minecraft/text/Text;ILnet/minecraft/client/TextRenderer;ZZ)Ljava/util/List;"))
+    //$$ private List<Text> filterDrawnTextComponents(Text p_178908_0_, int p_178908_1_, TextRenderer p_178908_2_, boolean p_178908_3_, boolean p_178908_4_, Operation<List<Text>> original) {
+    //$$    return ChatTabs.INSTANCE.shouldAllow(p_178908_0_) ? original.call(p_178908_0_, p_178908_1_, p_178908_2_, p_178908_3_, p_178908_4_) : Collections.emptyList();
+    //$$ }
+    //$$
+    //$$ @Unique
+    //$$ private static final ChatHudLine skytils$placeholderLine = new ChatHudLine(0, new LiteralTextContent("skytils placeholder"), 0);
+    //$$
+    //$$ @Inject(method = "getTextStyleAt", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/ChatHud;scrolledLines:I"), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
+    //$$ private void stopOutsideWindow(int mouseX, int mouseY, CallbackInfoReturnable<Text> cir, class_0_686 scaledresolution, int i, float f, int j, int k, int l) {
+    //$$    int line = k / this.client.textRenderer.field_0_2811;
+    //$$    if (line >= getVisibleLineCount()) cir.setReturnValue(null);
+    //$$ }
+    //$$ @Redirect(method = "addMessage(Lnet/minecraft/text/Text;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Text;getString()Ljava/lang/String;"))
+    //$$ private String printFormattedText(Text iChatComponent) {
+    //$$     return iChatComponent.method_10865();
+    //$$ }
+    //$$
+    //$$ @Inject(method = "removeMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ChatHudLine;getId()I"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    //$$ private void stopDeleteCrash(int id, CallbackInfo ci, Iterator<ChatHudLine> iterator, ChatHudLine chatline) {
+    //$$    if (chatline == null || skytils$placeholderLine == chatline) {
+    //$$        iterator.remove();
+    //$$    }
+    //$$ }
+    //$$
+    //$$ @ModifyVariable(method = "removeMessage", at = @At("STORE"))
+    //$$ private ChatHudLine stopDeleteCrash(ChatHudLine chatLine) {
+    //$$    return chatLine == null ? skytils$placeholderLine : chatLine;
+    //$$ }
+    //#else
+    @WrapMethod(method = "addVisibleMessage")
+    private void filterDrawnTextComponents(ChatHudLine message, Operation<Void> original) {
+        if (ChatTabs.INSTANCE.shouldAllow(message.content())) original.call(message);
     }
-
-    @Redirect(method = "addMessage(Lnet/minecraft/text/Text;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Text;getString()Ljava/lang/String;"))
-    private String printFormattedText(Text iChatComponent) {
-        return iChatComponent.method_10865();
-    }
-
-    @Inject(method = "getTextStyleAt", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/ChatHud;scrolledLines:I"), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void stopOutsideWindow(int mouseX, int mouseY, CallbackInfoReturnable<Text> cir, class_0_686 scaledresolution, int i, float f, int j, int k, int l) {
-        int line = k / this.client.textRenderer.field_0_2811;
-        if (line >= getVisibleLineCount()) cir.setReturnValue(null);
-    }
-
-    @Inject(method = "removeMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ChatHudLine;getId()I"), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void stopDeleteCrash(int id, CallbackInfo ci, Iterator<ChatHudLine> iterator, ChatHudLine chatline) {
-        if (chatline == null || skytils$placeholderLine == chatline) {
-            iterator.remove();
-        }
-    }
-
-    @ModifyVariable(method = "removeMessage", at = @At("STORE"))
-    private ChatHudLine stopDeleteCrash(ChatHudLine chatLine) {
-        return chatLine == null ? skytils$placeholderLine : chatLine;
-    }
+    //#endif
 }
