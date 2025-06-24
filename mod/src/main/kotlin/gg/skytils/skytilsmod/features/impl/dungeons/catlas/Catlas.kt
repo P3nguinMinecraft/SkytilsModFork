2097175d68d65a222dc2f4f71602ecb386db767e
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2025-06-24 15:48:56 -0400

    fix: Catlas wither door rendering is drawn at the wrong location
    Still has an issue where the whole rects aren't being rendered

--- mod/src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/catlas/Catlas.kt
+++ mod/src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/catlas/Catlas.kt
@@ -137,29 +137,27 @@ object Catlas : EventSubscriber {
         if (doors.isEmpty()) return
         val color = if (DungeonInfo.keys > 0) CatlasConfig.witherDoorKeyColor else CatlasConfig.witherDoorNoKeyColor
 
-        val matrices = UMatrixStack.Compat.get()
+        val matrices = UMatrixStack()
 
-        matrices.runWithGlobalState {
+        matrices.push()
+        DrawHelper.setupCameraTransformations(matrices)
+        val linesBuffer = UBufferBuilder.create(UGraphics.DrawMode.LINES, UGraphics.CommonVertexFormats.POSITION_COLOR)
+        val trianglesBuffer = UBufferBuilder.create(UGraphics.DrawMode.TRIANGLE_STRIP, UGraphics.CommonVertexFormats.POSITION_COLOR)
+        doors.forEach {
             matrices.push()
-            DrawHelper.setupCameraTransformations(matrices)
-            val linesBuffer = UBufferBuilder.create(UGraphics.DrawMode.LINES, UGraphics.CommonVertexFormats.POSITION_COLOR)
-            val trianglesBuffer = UBufferBuilder.create(UGraphics.DrawMode.TRIANGLES, UGraphics.CommonVertexFormats.POSITION_COLOR)
-            doors.forEach {
-                matrices.push()
-                matrices.translate(it.x.toDouble(), 0.0, it.z.toDouble())
-                DrawHelper.writeOutlineCube(
-                    linesBuffer,
-                    matrices,
-                    doorShape,
-                    color
-                )
-                DrawHelper.writeFilledCube(trianglesBuffer, matrices, doorShape, color.withAlpha(CatlasConfig.witherDoorFill))
-                matrices.pop()
-            }
-            linesBuffer.build()?.drawAndClose(SRenderPipelines.noDepthLinesPipeline)
-            trianglesBuffer.build()?.drawAndClose(SRenderPipelines.noDepthBoxPipeline)
+            matrices.translate(it.x.toDouble(), 0.0, it.z.toDouble())
+            DrawHelper.writeFilledCube(trianglesBuffer, matrices, doorShape, color.withAlpha(CatlasConfig.witherDoorFill))
+            DrawHelper.writeOutlineCube(
+                linesBuffer,
+                matrices,
+                doorShape,
+                color
+            )
             matrices.pop()
         }
+        linesBuffer.build()?.drawAndClose(SRenderPipelines.noDepthLinesPipeline)
+        trianglesBuffer.build()?.drawAndClose(SRenderPipelines.noDepthBoxPipeline)
+        matrices.pop()
     }
 
     fun onPuzzleReset(event: DungeonPuzzleResetEvent) {
