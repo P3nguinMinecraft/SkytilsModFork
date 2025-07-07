e7f373144be327a685892ea17fc26493b07f68c9
penguin <penguin586970@gmail.com>
2025-07-07 15:51:26 -0400

    apply logic in mixin, remove hook

--- mod/src/main/java/gg/skytils/skytilsmod/mixins/transformers/renderer/MixinPlayerEntityRenderer.java
+++ mod/src/main/java/gg/skytils/skytilsmod/mixins/transformers/renderer/MixinPlayerEntityRenderer.java
@@ -18,23 +18,31 @@
 package gg.skytils.skytilsmod.mixins.transformers.renderer;
 
 //#if MC>12000
-import gg.skytils.skytilsmod.mixins.hooks.renderer.PlayerEntityRendererHook;
+import gg.skytils.skytilsmod.utils.SuperSecretSettings;
+import gg.skytils.skytilsmod.utils.Utils;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Random;
+
 @Mixin(PlayerEntityRenderer.class)
 public class MixinPlayerEntityRenderer {
+    private static final Random random = new Random();
     @Inject(method = "scale(Lnet/minecraft/client/render/entity/state/PlayerEntityRenderState;Lnet/minecraft/client/util/math/MatrixStack;)V", at = @At("HEAD"))
     private static void doScale(PlayerEntityRenderState playerEntityRenderState, MatrixStack matrixStack, CallbackInfo ci){
-        if (PlayerEntityRendererHook.isSmol(playerEntityRenderState)){
+        if (isSmol(playerEntityRenderState)){
             matrixStack.scale(0.5f, 0.5f, 0.5f);
         }
+    }
+
+    private static boolean isSmol(PlayerEntityRenderState state){
+        boolean isBreefing = state.name.equals("Breefing") && (SuperSecretSettings.breefingDog || random.nextInt(100) < 3);
+        return Utils.INSTANCE.getInSkyblock() && (SuperSecretSettings.smolPeople || isBreefing);
     }
 }
 //#endif
