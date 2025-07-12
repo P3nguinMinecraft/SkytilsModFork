526337481dc62131a13a1a3fa89e5ff9b2dac1e9
sychic <47618543+Sychic@users.noreply.github.com>
2025-07-12 11:00:18 -0400

    refactor: remove deprecated `nbt` call

--- mod/src/main/kotlin/gg/skytils/skytilsmod/features/impl/misc/ItemFeatures.kt
+++ mod/src/main/kotlin/gg/skytils/skytilsmod/features/impl/misc/ItemFeatures.kt
@@ -579,7 +579,7 @@ object ItemFeatures : EventSubscriber {
 
     fun onRenderItemOverlayPost(event: GuiContainerPostDrawSlotEvent) {
         val item = event.slot.stack ?: return
-        if (!Utils.inSkyblock || item.count != 1 || item.get(DataComponentTypes.CUSTOM_DATA)?.nbt?.contains("SkytilsNoItemOverlay") == true) return
+        if (!Utils.inSkyblock || item.count != 1 || item.get(DataComponentTypes.CUSTOM_DATA)?.contains("SkytilsNoItemOverlay") == true) return
         val matrixStack = UMatrixStack()
         DrawHelper.setupContainerScreenTransformations(matrixStack, aboveItems = true)
         var stackTip = ""
