48f2a421ab12b8d235a28c5ec0a8299afe480769
penguin <136866320+P3nguinMinecraft@users.noreply.github.com>
2025-07-26 11:50:34 -0400

    Update mod/src/main/kotlin/gg/skytils/skytilsmod/utils/rendering/DrawHelper.kt
    Co-authored-by: Sychic <47618543+Sychic@users.noreply.github.com>
    Signed-off-by: penguin <136866320+P3nguinMinecraft@users.noreply.github.com>

--- mod/src/main/kotlin/gg/skytils/skytilsmod/utils/rendering/DrawHelper.kt
+++ mod/src/main/kotlin/gg/skytils/skytilsmod/utils/rendering/DrawHelper.kt
@@ -54,7 +54,7 @@ object DrawHelper {
      */
     fun cameraRotation(matrices: UMatrixStack) {
         //#if MC>12000
-        matrices.multiply(mc.gameRenderer.camera.rotation.conjugate(Quaternionf()).mul(70f/mc.options.fov.value))
+        matrices.multiply(mc.gameRenderer.camera.rotation.conjugate(Quaternionf()).mul(70f/mc.options.fov.value.coerceAtLeast(1))
         //#else
         //$$matrices.multiply(mc.gameRenderer.camera.rotation.conjugate(Quaternionf()))
         //#endif
