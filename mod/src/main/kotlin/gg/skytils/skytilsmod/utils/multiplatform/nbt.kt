08db25e6bc7a743fa4c833f00a423123b7bf32c3
penguin <penguin586970@gmail.com>
2025-08-07 01:11:04 +0800

    dont need this at all

--- mod/src/main/kotlin/gg/skytils/skytilsmod/utils/multiplatform/nbt.kt
+++ mod/src/main/kotlin/gg/skytils/skytilsmod/utils/multiplatform/nbt.kt
@@ -24,5 +24,5 @@ import kotlin.jvm.optionals.getOrNull
 
 val ItemStack.nbt
     get() =
-        if (this.isEmpty) null
-        else MinecraftClient.getInstance().player?.registryManager?.let(this::toNbt)?.asCompound()?.getOrNull()
+        if (isEmpty) null
+        else MinecraftClient.getInstance().player?.registryManager?.let(::toNbt)?.asCompound()?.getOrNull()
